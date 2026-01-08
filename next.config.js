2fd532e20be3249ede169dc53487c348853ae9f8
Nandan Shrivastava <nandan.shri21@gmail.com>
2026-01-08 06:29:59 +0530

    done

--- next.config.js
+++ next.config.js
@@ -3,16 +3,16 @@ module.exports = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'ui-avatars.com',
+        protocol: "https",
+        hostname: "ui-avatars.com",
       },
       {
-        protocol: 'https',
-        hostname: 'inrext-backend.vercel.app',
+        protocol: "https",
+        hostname: "inrext-backend.vercel.app",
       },
       {
-        protocol: 'https',
-        hostname: 'inrext.s3.ap-south-1.amazonaws.com',
+        protocol: "https",
+        hostname: "inrext.s3.ap-south-1.amazonaws.com",
       },
     ],
   },
